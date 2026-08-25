3d151ebdb2dcb56e5514df01c78afe8199f67f85
HIkaru Furuta <89366952+cureflash@users.noreply.github.com>
2026-08-25 20:36:04 +0900

    Add world capital audio config

---
+++ games/world-capitals/audio.js
@@ -0,0 +1,7 @@
+export const AUDIO_CONFIG = {
+  bgm: "./audio/bgm.mp3",
+  select: "./audio/select.mp3",
+  correct: "./audio/correct.mp3",
+  wrong: "./audio/wrong.mp3",
+  clear: "./audio/clear.mp3",
+};
