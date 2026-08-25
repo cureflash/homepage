f433fc7ba2920dc9c8e7169da4995dbad7773b12
HIkaru Furuta <89366952+cureflash@users.noreply.github.com>
2026-08-25 20:01:45 +0900

    Move old maid card data to dedicated path

---
+++ games/old-maid/cards.js
@@ -0,0 +1,15 @@
+export const OLD_MAID_CONFIG = {
+  pairCount: 4,
+  handSize: 9,
+  swipeThreshold: 32,
+};
+
+export const RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+export const SUITS = ["♥", "♦", "♠", "♣"];
+
+export const JOKER = {
+  id: "joker",
+  rank: "JOKER",
+  suit: "★",
+  joker: true,
+};
