bad82f13dd071a2c873b8505cd03b92b9f16019e
HIkaru Furuta <89366952+cureflash@users.noreply.github.com>
2026-08-25 22:21:24 +0900

    Add social quiz game registry

---
+++ subjects/social/quiz/js/games/registry.js
@@ -0,0 +1,28 @@
+import { assertValidGameDefinition } from "../core/game-validator.js";
+import { prefectureGame } from "./prefectures.js";
+
+const registeredGames = [prefectureGame].map((game) => assertValidGameDefinition(game));
+const gamesById = new Map();
+
+for (const game of registeredGames) {
+  if (gamesById.has(game.id)) {
+    throw new Error(`Duplicate registered game id: ${game.id}`);
+  }
+  gamesById.set(game.id, game);
+}
+
+export const defaultGameId = prefectureGame.id;
+
+export function listGames() {
+  return [...registeredGames];
+}
+
+export function getGame(gameId = defaultGameId) {
+  return gamesById.get(gameId) ?? null;
+}
+
+export function requireGame(gameId = defaultGameId) {
+  const game = getGame(gameId);
+  if (!game) throw new Error(`Unknown game id: ${gameId}`);
+  return game;
+}
