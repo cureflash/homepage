8645fad6b02cb3d43f27d48b6cf409dc4d2403a8
HIkaru Furuta <89366952+cureflash@users.noreply.github.com>
2026-08-26 18:18:48 +0900

    Validate world quiz public entry point

--- subjects/social/quiz/tests/public-entrypoints.test.js
+++ subjects/social/quiz/tests/public-entrypoints.test.js
@@ -5,12 +5,15 @@ import test from "node:test";
 const rootIndex = readFileSync(new URL("../../../../index.html", import.meta.url), "utf8");
 const quizIndex = readFileSync(new URL("../index.html", import.meta.url), "utf8");
 
-test("root learning-game section links to all Japan prefecture/capital drills", () => {
+test("root learning-game section links to Japan drills and the world country game family", () => {
   assert.match(rootIndex, /href="subjects\/social\/quiz\/"/);
   assert.match(rootIndex, /href="subjects\/social\/quiz\/\?game=japan-prefectural-capitals"/);
   assert.match(rootIndex, /href="subjects\/social\/quiz\/\?game=japan-prefecture-capital-choice"/);
+  assert.match(rootIndex, /href="subjects\/social\/quiz\/\?game=world-countries"/);
 });
 
-test("canonical social quiz page still loads the shared main module", () => {
+test("canonical social quiz page loads shared main module and world selectors", () => {
   assert.match(quizIndex, /src="js\/main\.js"/);
+  assert.match(quizIndex, /id="world-region-select"/);
+  assert.match(quizIndex, /id="world-mode-select"/);
 });
