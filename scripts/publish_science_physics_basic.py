b2be69b79510893f73c1e043165d93d1a452655f
HIkaru Furuta <89366952+cureflash@users.noreply.github.com>
2026-08-27 09:01:38 +0900

    Wire Physics Basics beats into publisher

--- scripts/publish_science_physics_basic.py
+++ scripts/publish_science_physics_basic.py
@@ -5,6 +5,7 @@ import tempfile
 from pathlib import Path
 
 try:
+    from scripts.science_physics_basic_beats import PHYSICS_BASIC_BEATS_TOPICS
     from scripts.science_physics_basic_conservation import PHYSICS_BASIC_CONSERVATION_TOPICS
     from scripts.science_physics_basic_energy import PHYSICS_BASIC_ENERGY_TOPICS
     from scripts.science_physics_basic_forces import PHYSICS_BASIC_FORCE_TOPICS
@@ -19,6 +20,7 @@ try:
     from scripts.science_worksheet_helpers import generate_formula_drill
     from scripts.worksheet_factory import normalized_hash, render_pdf, validate, validate_catalog
 except ModuleNotFoundError:
+    from science_physics_basic_beats import PHYSICS_BASIC_BEATS_TOPICS
     from science_physics_basic_conservation import PHYSICS_BASIC_CONSERVATION_TOPICS
     from science_physics_basic_energy import PHYSICS_BASIC_ENERGY_TOPICS
     from science_physics_basic_forces import PHYSICS_BASIC_FORCE_TOPICS
@@ -45,6 +47,7 @@ ALL_TOPICS = {
     **PHYSICS_BASIC_WAVE_TOPICS,
     **PHYSICS_BASIC_SOUND_TOPICS,
     **PHYSICS_BASIC_STRING_TOPICS,
+    **PHYSICS_BASIC_BEATS_TOPICS,
 }
 
 
